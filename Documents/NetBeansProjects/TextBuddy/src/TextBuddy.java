6c636fb50eeb69e8d30930c61f526cbf35f69e72
yaoxiang <teryaoxiang@gmail.com>
2014-08-24 21:48:37 +0800

    Updated to lastest format

---
+++ Documents/NetBeansProjects/TextBuddy/src/TextBuddy.java
@@ -0,0 +1,166 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * This class run a program which allows user to use operations such as add, display, delete, clear, exit on a text file.
+ * User's input will be saved into a .txt file.
+ * To run: java -jar textbuddy.jar [file name]
+ *    e.g: java -jar textbuddy.jar myfile.txt
+ * 
+ * @author Ter Yao Xiang A0110751W
+ */
+
+public class TextBuddy {
+
+    public static String CURR_DIR = System.getProperty("user.dir") + "\\";
+    public static BufferedWriter writer;
+    public static BufferedReader reader;
+    
+    public static void main(String[] args) throws IOException {
+        createFileIfNotExist(args[0]);
+        run(args[0]);
+    }
+    
+    public static void createFileIfNotExist(String fileName) throws IOException {
+        File textFile = new File(CURR_DIR + fileName);
+        
+        if (!textFile.exists())
+            textFile.createNewFile();
+    }
+    
+    private static void run(String fileName) throws IOException {
+        System.out.println("Welcome to TextBuddy. " + fileName + " is ready for use");
+        
+        Scanner sc = new Scanner(System.in);
+        String[] input;
+        
+        do {
+            System.out.print("command: ");
+            input = sc.nextLine().split(" ");
+            try {
+                processCommand(fileName, input);
+            } catch (IOException e) {
+                System.exit(1);
+            }
+        } while (true);
+    }
+    
+    private static void processCommand(String fileName, String[] input) throws IOException {
+        switch (input[0]) {
+            case "add":
+                addString(fileName, input);
+                break;
+                
+            case "display":
+                displayFile(fileName);
+                break;
+                
+            case "delete":
+                deleteLine(fileName, input);
+                break;
+                
+            case "clear":
+                clearTextFile(fileName);
+                break;
+                
+            case "exit":
+                System.exit(0);
+                break;
+                
+            default:
+                System.out.println("Wrong command!");
+                break;
+        }
+    }
+    
+    //This method append a new line of string to the end of the text file
+    private static void addString(String fileName, String[] input) throws IOException {
+        String fileLocation = CURR_DIR + fileName;
+        writer = new BufferedWriter(new FileWriter(fileLocation, true));
+        
+        if (input.length > 1) {
+            String strAdd = input[1];
+            for (int i = 2; i < input.length; i++) {
+                strAdd = strAdd + " " + input[i];
+            }
+            writer.write(strAdd);
+            writer.newLine();
+            
+            System.out.println("added to " + fileName + ": \"" + strAdd + "\"");
+        }
+        
+        writer.close();
+    }
+
+    //This method displays all lines of text found in the text file, numbered.
+    private static void displayFile(String fileName) throws IOException {
+        String fileLocation = CURR_DIR + fileName;
+        reader = new BufferedReader(new FileReader(fileLocation));
+        
+        int i = 1;
+        String currLine = reader.readLine();
+        while (currLine != null) {
+            System.out.println(i + ". " + currLine);
+            currLine = reader.readLine();
+            i++;
+        }
+        if (i == 1) {
+            System.out.println(fileName + " is empty");
+        }
+        reader.close();
+    }
+
+    //This method will delete the specified line in the text file.
+    //Deletion is done in a temporary file before having it renamed and replace the original file.
+    private static void deleteLine(String fileName, String[] input) throws IOException {
+        
+        String fileLocation = CURR_DIR + fileName;
+        File textFile = new File(fileLocation);
+        File tempFile = new File(fileLocation + ".tmp");
+        reader = new BufferedReader(new FileReader(fileName));
+        writer = new BufferedWriter(new FileWriter(fileName + ".tmp"));
+        
+        Integer lineDelete = Integer.parseInt(input[1]);
+        
+        String writeThis, strDeleted = "";
+        
+        for (int j = 1; j > 0; j++) {
+            writeThis = reader.readLine();
+            if (lineDelete != j) {
+                if (writeThis != null) {
+                    writer.write(writeThis);
+                    writer.newLine();
+                } else {
+                    System.out.println("deleted from " + fileName + ": \"" + strDeleted + "\"");
+                    break;
+                }
+            } else {
+                strDeleted = writeThis;
+            }
+        }
+        
+        try {
+            writer.close();
+            reader.close();
+            textFile.delete();
+            tempFile.renameTo(textFile);
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    //This method resets the text file by deleting the original file and creates a new one
+    private static void clearTextFile(String fileName) throws IOException {
+        String fileLocation = CURR_DIR + fileName;
+        File textFile = new File(fileLocation);
+        textFile.delete();
+        System.out.println("all content deleted from " + fileName);
+        createFileIfNotExist(fileName);
+    }
+}
